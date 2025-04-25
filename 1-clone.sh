bc5168f5d6574fd50d1f1cddbd8e9a2fc09b30ce
David Martins de Matos <david.m.matos@gmail.com>
2025-04-25 19:48:14 +0100

    Minor change.

--- 1-clone.sh
+++ 1-clone.sh
@@ -10,7 +10,7 @@ done
 
 cp -a checked-out checked-out-ref
 #for i in {000..012} {014..041} {043..067} {069..094} {096..100} {102..120} 122 124 {126..131} 138 {181..182} {190..200}; do
-for i in 000..199}; do
+for i in {000..199}; do
   (cd checked-out-ref/$i; git -c advice.detachedHead=false checkout `git rev-list -1 --date=iso --before="2024-05-24T20:10:00+00:00" master`)
 done
 
